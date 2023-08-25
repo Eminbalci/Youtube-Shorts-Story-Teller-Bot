19f10462068ea8e916627718159adee3c748aacc
Muhammet Emin Balcıoğlu <50174379+Eminbalci@users.noreply.github.com>
2023-08-25 15:22:42 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,22 +4,7 @@ import random
 from datetime import datetime
 from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip, AudioFileClip, vfx, concatenate_videoclips
 
-# Get text input from the user
-text = input("Enter the text to narrate: ")
-sentences = text.split(". ")  # Split the text into sentences
-
-# Define the output folder for temporary files created by libraries
-output_folder = r"C:\output"  # You can specify the video output folder here
-os.makedirs(output_folder, exist_ok=True)
-
-# Select a random video
-video_folder = r"C:\videos"
-video_list = os.listdir(video_folder)
-selected_video = random.choice(video_list)
-background_video = VideoFileClip(os.path.join(video_folder, selected_video))
-
-# Code used to edit the background video
-def edit_background(video):
+def duzenle_arkaplan(video):
     if video.aspect_ratio != (9, 16) and video.size[0] > video.size[1]:
         new_width = video.size[1] * 9 // 16
         video = video.crop(x_center=(video.size[0] - new_width) * 7 / 10, width=new_width)
@@ -28,68 +13,97 @@ def edit_background(video):
     video_duration = min(59, video.duration)
     return video, frame_rate, frame_width, frame_height, video_duration
 
-background_video, frame_rate, frame_width, frame_height, background_duration = edit_background(background_video)
+def create_video(video_clips, output_folder):
+    if not video_clips:
+        return
 
-# Processing for each sentence
+    final_video = concatenate_videoclips(video_clips)
+
+    now = datetime.now()
+    output_file_name = now.strftime("%Y-%m-%d_%H-%M-%S") + ".mp4"
+    output_file = os.path.join(output_folder, output_file_name)
+    final_video.write_videofile(output_file, codec="libx265", audio_codec="aac")
+
+    print(f"Video başarıyla oluşturuldu: {output_file}")
+    print("Çıktı Dosyasının Yolu:", output_file)
+
+def generate_audio_files(cumleler, output_folder):
+    for idx, cumle in enumerate(cumleler):
+        ses_file = os.path.join(output_folder, f"ses_{idx + 1}.mp3")  # Sıfırdan başlamıyor
+        tts = gTTS(cumle, lang="en")
+        tts.save(ses_file)
+
+def delete_audio_files(output_folder, num_files):
+    for idx in range(1, num_files + 1):
+        ses_file_path = os.path.join(output_folder, f"ses_{idx}.mp3")
+        if os.path.exists(ses_file_path):
+            os.remove(ses_file_path)
+
+metin = input("Seslendirmek istediğiniz metni girin: ")
+cumleler = metin.split(". ")  # Metni cümlelere bölelim
+
+output_folder = r"C:\cikti"
+os.makedirs(output_folder, exist_ok=True)
+
+# Ses dosyalarını oluştur
+generate_audio_files(cumleler, output_folder)
+
+video_klasoru = r"C:\video"
+video_listesi = os.listdir(video_klasoru)
+
 video_clips = []
-background_offset = 0
+arkaplan_offset = 0
 
-for idx, sentence in enumerate(sentences):
-    # Generate speech for the sentence
-    audio_file = os.path.join(output_folder, f"audio_{idx}.mp3")
-    tts = gTTS(sentence, lang="en")  # English text-to-speech
-    tts.save(audio_file)
+# Rastgele arkaplan video seçimi
+arkaplan_video = VideoFileClip(os.path.join(video_klasoru, random.choice(video_listesi)))
+arkaplan_video, frame_rate, frame_width, frame_height, _ = duzenle_arkaplan(arkaplan_video)
 
-    # Calculate sentence duration
-    audio_clip = AudioFileClip(audio_file)
-    sentence_duration = audio_clip.duration
+for idx, cumle in enumerate(cumleler):
+    # Cümlenin süresini hesaplama
+    ses_clip = AudioFileClip(os.path.join(output_folder, f"ses_{idx + 1}.mp3"))
+    cumle_suresi = ses_clip.duration
 
-    # Edit the video
-    background_clip = background_video.subclip(background_offset, background_offset + sentence_duration)
-    background_clip, _, _, _, _ = edit_background(background_clip)
+    # Videonun süresi 59 saniyeyi geçerse mevcut videoyu tamamlayıp yeni bir video oluştur
+    if arkaplan_offset + cumle_suresi > 59:
+        create_video(video_clips, output_folder)
+        video_clips = []
+        arkaplan_offset = 0
 
-    video_duration = sentence_duration
+        # Rastgele arkaplan video seçimi (yeni bir video başladığında)
+        arkaplan_video = VideoFileClip(os.path.join(video_klasoru, random.choice(video_listesi)))
+        arkaplan_video, _, _, _, _ = duzenle_arkaplan(arkaplan_video)
 
-    # Trim and adjust the audio file
-    sped_up_audio_clip = audio_clip.fx(vfx.speedx, video_duration / sentence_duration)
-    sped_up_audio_clip = sped_up_audio_clip.set_duration(video_duration)
+    arkaplan_clip = arkaplan_video.subclip(arkaplan_offset, arkaplan_offset + cumle_suresi)
+    arkaplan_clip, _, _, _, _ = duzenle_arkaplan(arkaplan_clip)
 
-    # Format the sentence as text overlay
-    text_clip = TextClip(
-        sentence,
+    video_duration = cumle_suresi
+
+    ses_clip_speedup = ses_clip.set_duration(video_duration)
+
+    txt_clip = TextClip(
+        cumle,
         color="white",
-        fontsize=20,
+        fontsize=30,
         size=(frame_width - 175, None),
         method="caption",
         align="center",
         kerning=0,
         interline=-5,
-        bg_color="rgba(0, 0, 0, 0.5)"
+        bg_color="rgba(0, 0, 0, 0.5)",
     )
-    text_clip = text_clip.set_duration(video_duration)
+    txt_clip = txt_clip.set_duration(video_duration)
 
-    # Position the text in the center of the screen
-    text_clip = text_clip.set_position(("center", "center"))
+    y_position = frame_height // 2 + 100
+    txt_clip = txt_clip.set_position(("center", y_position))
 
-    # Combine video and text
-    video_with_text = CompositeVideoClip([background_clip.set_duration(video_duration), text_clip])
-    video_with_audio = video_with_text.set_audio(sped_up_audio_clip)
+    video_with_text = CompositeVideoClip([arkaplan_clip.set_duration(video_duration), txt_clip])
+    video_with_audio = video_with_text.set_audio(ses_clip_speedup)
 
     video_clips.append(video_with_audio)
-    background_offset += sentence_duration  # Update the starting point of the background
+    arkaplan_offset += cumle_suresi
 
-# Concatenate all video clips
-final_video = concatenate_videoclips(video_clips)
+# Kapatmadan önce son videoyu tamamlayalım
+create_video(video_clips, output_folder)
 
-# Save the result
-now = datetime.now()
-output_file_name = now.strftime("%Y-%m-%d_%H-%M-%S") + ".mp4"
-output_file = os.path.join(output_folder, output_file_name)
-final_video.write_videofile(output_file, codec="libx265", audio_codec="aac")
-
-# Delete unused audio files
-for idx in range(len(sentences)):
-    os.remove(os.path.join(output_folder, f"audio_{idx}.mp3"))
-
-print(f"Video successfully created: {output_file}")
-print("Output File Path:", output_file)
+# Tüm videolar oluşturulduktan sonra ses dosyalarını silme
+delete_audio_files(output_folder, len(cumleler))
